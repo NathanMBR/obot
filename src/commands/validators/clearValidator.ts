5ed1572de92458553b32686aaedf73e2eccadbb8
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-02-05 20:50:05 -0300

    chore: add clear command validator

---
+++ src/commands/validators/clearValidator.ts
@@ -0,0 +1,23 @@
+import { Message, Permissions } from "discord.js";
+import { ValidationError } from "@/errors";
+
+export const clearValidator = (quantity: number): void => {
+	if (
+		typeof quantity !== "number" ||
+		Number.isNaN(quantity)
+	)
+		throw new ValidationError("Invalid number argument.");
+
+	if (quantity <= 0)
+		throw new ValidationError("The quantity must be greater than zero.");
+
+	if (quantity > 20)
+		throw new ValidationError("The quantity must be less than or equal to 20.");
+};
+
+export const nonMockableClearValidator = (message: Message<boolean>) => {
+	if (
+		!message.member?.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)
+	)
+		throw new ValidationError("You don't have the necessary permissions to use this command.");
+};
