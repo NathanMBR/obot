637902633865e2db78f5e6268a52641166244e43
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-02-05 16:47:46 -0300

    chore: add ready event handler

---
+++ src/events/ready.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+import { Client } from "discord.js";
+
+export const handleReady = (client: Client<boolean>) =>
+	() => {
+		if (client.user) {
+			const { username, discriminator } = client.user;
+			console.log(`Successfully logged in as ${username}#${discriminator}`);
+		} else {
+			console.error("The bot couldn't be initialized.");
+			process.exit(1);
+		}
+	};
