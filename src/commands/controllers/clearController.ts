72476a13ace400e03c8af8c06161b57dfbe426d9
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-02-05 22:47:23 -0300

    chore: add clear command controller

---
+++ src/commands/controllers/clearController.ts
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+
+import { IControllerInjection } from "@/types";
+import {
+	clearValidator,
+	nonMockableClearValidator,
+	clearResource,
+	clearService
+} from "@/commands";
+import { ChannelLogsQueryOptions } from "discord.js";
+import { ValidationError } from "@/errors";
+
+export const clearController = async ({ message, splitCommand }: IControllerInjection) => {
+	try {
+		const messagesQuantity = Number(splitCommand[2]);
+		const searchOptions: ChannelLogsQueryOptions = {
+			limit: messagesQuantity
+		};
+
+		clearValidator(messagesQuantity);
+		nonMockableClearValidator(message);
+
+		const fetchedMessages = await clearResource(message, searchOptions);
+		clearService(fetchedMessages);
+	} catch (error) {
+		if (error instanceof ValidationError)
+			message.reply(error.message);
+		else {
+			console.error("An internal error occurred. Error:\n", error);
+			message.reply("An internal error occurred and the request can't be successfully finished.");
+		}
+	}
+};
