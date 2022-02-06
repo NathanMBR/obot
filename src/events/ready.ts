14ec9cf573ab59ed298e50adf878067e127c100e
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-02-06 12:17:25 -0300

    refactor: change ready event handling strategy

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,13 +1,12 @@
 /* eslint-disable no-console */
 import { Client } from "discord.js";
 
-export const handleReady = (client: Client<boolean>) =>
-	() => {
-		if (client.user) {
-			const { username, discriminator } = client.user;
-			console.log(`Successfully logged in as ${username}#${discriminator}`);
-		} else {
-			console.error("The bot couldn't be initialized.");
-			process.exit(1);
-		}
-	};
+export const handleReady = (client: Client<boolean>) => {
+	if (client.user) {
+		const { username, discriminator } = client.user;
+		console.log(`Successfully logged in as ${username}#${discriminator}`);
+	} else {
+		console.error("The bot couldn't be initialized.");
+		process.exit(1);
+	}
+};
--- src/helpers/setupEventListeners.ts
+++ src/helpers/setupEventListeners.ts
@@ -1,10 +1,12 @@
 import { Client } from "discord.js";
 import {
 	handleReady,
-	handleError
+	handleError,
+	handleMessageCreate
 } from "@/events";
 
 export const setupEventListeners = (client: Client<boolean>) => {
-	client.once("ready", handleReady(client));
+	client.once("ready", handleReady);
 	client.on("error", handleError);
+	client.on("messageCreate", handleMessageCreate);
 };
