bc150263424b38f3e4312d372d4cde4dbda790c2
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-02-06 11:52:21 -0300

    feat: add default command

--- src/commands/commandsList.ts
+++ src/commands/commandsList.ts
@@ -1,8 +1,10 @@
 import { ICommandsList } from "@/types";
 import {
+	defaultController,
 	clearController
 } from "@/commands";
 
 export const commandsList: ICommandsList = {
+	default: defaultController,
 	clear: clearController
 };
